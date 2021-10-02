9d821953143eb1659cce629293aa8b9603bb9af3
AllanCapistrano <allan.capistrano3@gmail.com>
2021-10-02 17:11:21 -0300

    Add time in milliseconds as unique identifier.

--- src/main/FogHandler.java
+++ src/main/FogHandler.java
@@ -9,19 +9,24 @@ import mqtt.MQTTClient;
  * @author Allan Capistrano e João Erick Barbosa
  */
 public class FogHandler implements Runnable {
-    
+
     private static final String MQTT_ADDRESS = "tcp://broker.mqttdashboard.com:1883";
     private static final String DEFAULT_SUBSCRIBE_TOPIC = "tec502/pbl2/fog";
     private static final int QOS = 0;
-    
+
     private final String clientTopic;
 
+    /**
+     * Método construtor.
+     *
+     * @param fogId int - Identificador da fog.
+     */
     public FogHandler(int fogId) {
         MQTTClient clienteMQTT = new MQTTClient(MQTT_ADDRESS, null, null);
         clienteMQTT.connect();
-        
-        this.clientTopic = DEFAULT_SUBSCRIBE_TOPIC + "/" + fogId;
-        
+
+        this.clientTopic = DEFAULT_SUBSCRIBE_TOPIC + "/" + System.currentTimeMillis() + "/" + fogId;
+
         new FogListener(clienteMQTT, DEFAULT_SUBSCRIBE_TOPIC, QOS, this.clientTopic);
         new FogListener(clienteMQTT, this.clientTopic, QOS);
     }
@@ -29,5 +34,5 @@ public class FogHandler implements Runnable {
     @Override
     public void run() {
     }
-    
+
 }
