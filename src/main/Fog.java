fa5336c30d3f675be61f71d36851249afcd9809f
JoaoErick <jerick1415@outlook.com>
2021-10-26 17:21:29 -0300

    Remove proccess requests.

--- src/main/Fog.java
+++ src/main/Fog.java
@@ -1,14 +1,10 @@
 package main;
 
-import utils.PatientToJson;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.BindException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,7 +14,6 @@ import java.util.concurrent.Executors;
 import models.PatientDevice;
 import mqtt.FogListener;
 import mqtt.MQTTClient;
-import org.json.JSONObject;
 
 /**
  * Fog responsável pela comunicação com os dispositivos e com o servidor.
@@ -32,18 +27,18 @@ public class Fog {
     public static final String MQTT_ADDRESS = "tcp://broker.mqttdashboard.com:1883";
     public static final String SOCKET_ADDRESS = "localhost";
     public static int SOCKET_PORT = 12240;
-    private static final int SLEEP = 5000;
     private static final String DEFAULT_TOPIC = "tec502/pbl2/fog/";
     private static final int QOS = 0;
     /*------------------------------------------------------------------------*/
 
     private static final String regions[]
-            = {"Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"};
+            = {"Norte", "Sul"};
 
-    private static final List<PatientDevice> patientDevices
+    public static final List<PatientDevice> patientDevices
             = Collections.synchronizedList(new ArrayList());
 
     private static final ArrayList<FogHandler> fogHandler = new ArrayList<>();
+    private static final ArrayList<FogRequestHandler> fogRequest = new ArrayList<>();
     private static final ExecutorService pool = Executors.newCachedThreadPool();
 
     private static int listLength;
@@ -60,10 +55,7 @@ public class Fog {
 
         System.out.println("Digite o número que corresponde a região dessa Fog: ");
         System.out.println("1 - Norte");
-        System.out.println("2 - Nordeste");
-        System.out.println("3 - Centro-Oeste");
-        System.out.println("4 - Sudeste");
-        System.out.println("5 - Sul");
+        System.out.println("2 - Sul");
         System.out.print("> ");
 
         try {
@@ -203,41 +195,6 @@ public class Fog {
     }
 
     /**
-     * Envia para o servidor uma requisição.
-     *
-     * @param httpMethod String - Método HTTP da requisição que será feita.
-     * @param route String - Rota para a qual a requisição será feita.
-     * @param amount int - Quantidade de dispositivos de pacientes.
-     * @param conn Socket - Conexão que é realizada com o servidor.
-     */
-    public static void sendToServer(
-            String httpMethod,
-            String route,
-            int amount,
-            Socket conn
-    ) {
-        amount = amount > Fog.patientDeviceListSize()
-                ? Fog.patientDeviceListSize()
-                : amount;
-
-        JSONObject json = patientsDevicesToJSON(httpMethod, route, amount);
-
-        try {
-            ObjectOutputStream output
-                    = new ObjectOutputStream(conn.getOutputStream());
-
-            /* Enviando a requisição para o servidor. */
-            output.writeObject(json);
-
-            output.close();
-        } catch (IOException ioe) {
-            System.err.println("Erro ao tentar enviar os dados dos sensores "
-                    + "para o servidor.");
-            System.out.println(ioe);
-        }
-    }
-
-    /**
      * Inicializa o servidor da Fog.
      */
     public static void initializeServerFog() {
@@ -266,45 +223,18 @@ public class Fog {
                     try {
                         /* Caso tenha algum dispositivo para ser enviado para o servidor. */
                         if (patientDeviceListSize() > 0) {
-                            Socket connection = serverFog.accept();
+                            
+                            /* Serviço que lida com as requisições utilizando threads. */
+                            FogRequestHandler requestHandler = new FogRequestHandler(serverFog.accept());
+                            fogRequest.add(requestHandler);
 
-                            ObjectInputStream input
-                                    = new ObjectInputStream(connection.getInputStream());
-
-                            JSONObject request = (JSONObject) input.readObject();
-
-                            if (request
-                                    .getString("method")
-                                    .equals("GET")
-                                    && request
-                                            .getString("route")
-                                            .contains("/patients")) {
-
-                                String[] temp = request.getString("route").split("/");
-
-                                sendToServer(
-                                        "POST",
-                                        "/patients",
-                                        Integer.parseInt(temp[2]),
-                                        connection
-                                );
-                            }
-
-                            connection.close();
+                            /* Executando as threads. */
+                            pool.execute(requestHandler);
                         }
-
-                        Thread.sleep(SLEEP);
-                    } catch (ClassNotFoundException cnfe) {
-                        System.err.println("Servidor não encontrado ou "
-                                + "está fora do ar.");
-                        System.out.println(cnfe);
                     } catch (IOException ioe) {
                         System.err.println("Classe JSONObject não foi "
                                 + "encontrada.");
                         System.out.println(ioe);
-                    } catch (InterruptedException ie) {
-                        System.err.println("Não foi possível parar a Thread");
-                        System.out.println(ie);
                     }
                 }
             }
@@ -314,23 +244,6 @@ public class Fog {
         thread.setDaemon(true);
         /* Iniciar a thread de requisições. */
         thread.start();
-    }
-
-    /**
-     * Monta um JSON com as dados de um número específicos de pacientes.
-     *
-     * @param httpMethod String - Método HTTP da requisição que será feita.
-     * @param route String - Rota para a qual a requisição será feita.
-     * @param amount int - Quantidade de dispositivos de pacientes.
-     */
-    private static JSONObject patientsDevicesToJSON(
-            String httpMethod,
-            String route,
-            int amount
-    ) {
-        List<PatientDevice> temp = patientDevices;
-
-        return PatientToJson.handle(temp, httpMethod, route, amount);
     }
 
 }
